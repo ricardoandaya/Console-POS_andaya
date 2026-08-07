de71866fafadcf35cd7d57f74f97f57d7a2021a6
gelo568 <raquel@RENRENNN>
2026-08-07 16:08:38 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_POS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMenu();
+
+            Console.ReadKey();
+        }
+
+        private static void DisplayMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("    Welcome to the POS System  ");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("[1] Add Item");
+            Console.WriteLine("[2] Remove Item");
+            Console.WriteLine("[3] View Cart");
+            Console.WriteLine("[4] Checkout");
+            Console.WriteLine("[5] Exit");
+            Console.WriteLine("==============================");
+            Console.Write("Please select an option: ");
+        }
+    }
+}
